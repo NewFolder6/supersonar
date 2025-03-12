121b5d1f2b30b624c400bf1f38ca360858555851
New Folder (6) <150495167+NewFolder6@users.noreply.github.com>
2025-03-12 00:12:08 -0700

    Update package.json with new dependencies and enhance trainModel.js with error handling instructions

--- ml/trainModel.js
+++ ml/trainModel.js
@@ -4,3 +4,6 @@ const x = tf.tensor1d([0, 1, 2, 3, 4]);
 const y = x.mul(tf.scalar(2)).add(tf.scalar(1));
 console.log('x:', x.arraySync());
 console.log('y:', y.arraySync());
+
+//move \node_modules\@tensorflow\tfjs-node\deps\lib\tensorflow.dll To \node_modules\@tensorflow\tfjs-node\lib\napi-v8\
+// if error with 'ERR_DLOPE_FAILED' or 'ERR_MODULE_NOT_FOUND'
